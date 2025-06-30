013b8310fa2cae94d959d23476dc41bac2fc70d7
sdw98 <sdw9851@gmail.com>
2025-06-30 09:15:17 +0900

    first

---
+++ src/main/java/com/sdw/toDoList/ToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.sdw.toDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListApplication.class, args);
+	}
+
+}
